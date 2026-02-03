44f9bff7eaccd9de6f77f7a64eb73b0c28edc03b
so <tamaki.sou@outlook.jp>
2026-02-04 01:30:46 +0900

    一旦使えるように修正

--- test/simple/server/run.sh
+++ test/simple/server/run.sh
@@ -4,5 +4,12 @@ ip mptcp limits set subflow 1
 ip mptcp endpoint add 10.123.201.2 dev eth1 signal
 
 nginx
-iperf3 -s &
+
+# Start iperf3 in background
+iperf3 -s -p 5201 &
+
+# Wait a bit for iperf3 to start
+sleep 1
+
+# Start mptcp-proxy (this will run in foreground)
 /mptcp-proxy/mptcp-proxy -m server -p 4444 -r localhost:5201
